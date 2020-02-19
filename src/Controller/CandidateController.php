1c246c9a8cd076213cf7bfecf31f961e68f9b8f1
Tony Blard <tonyblard55@gmail.com>
2020-02-19 10:30:00 +0100

    create form and security access page

--- src/Controller/CandidateController.php
+++ src/Controller/CandidateController.php
@@ -6,6 +6,7 @@ use App\Entity\Candidate;
 use App\Entity\Result;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CandidateController extends AbstractController
@@ -28,13 +29,16 @@ class CandidateController extends AbstractController
     /**
      * @Route("/candidate/{id}", name="one_candidate")
      */
-    public function getCandidate(int $id)
+    public function getCandidate(int $id, Request $request)
     {
 
         $candidate = $this->getDoctrine()->getRepository(Candidate::class)->find($id);
 
         $results = $this->getDoctrine()->getRepository(Result::class)->findBy(['candidate' => $candidate]);
 
+        if ($request->isMethod('POST')) {
+
+        }
 
         return $this->render('candidate/oneCandidate.html.twig', [
             'candidate' => $candidate,
