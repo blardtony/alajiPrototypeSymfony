cc6984bc4b5d49a6d6e5e86aca2c869dadc9bd7d
Tony Blard <tonyblard55@gmail.com>
2020-02-19 11:14:41 +0100

    form completed

--- src/Controller/CandidateController.php
+++ src/Controller/CandidateController.php
@@ -32,17 +32,51 @@ class CandidateController extends AbstractController
     public function getCandidate(int $id, Request $request)
     {
 
+
         $candidate = $this->getDoctrine()->getRepository(Candidate::class)->find($id);
 
-        $results = $this->getDoctrine()->getRepository(Result::class)->findBy(['candidate' => $candidate]);
-
         if ($request->isMethod('POST')) {
+            $submittedToken = $request->request->get('token');
+            if ($this->isCsrfTokenValid('addCriteria', $submittedToken)) {
+                $oral1 = intval($_POST['criteria1']);
+                $oral2 = intval($_POST['criteria2']);
+                $oral3 = intval($_POST['criteria3']);
+                $oral4 = intval($_POST['criteria4']);
 
+                $results = $this->getDoctrine()->getRepository(Result::class)->findOneBy(['candidate' => $candidate, 'criteria' => 1]);
+                $results->setOralreview($oral1);
+                $manager = $this->getDoctrine()->getManager();
+                $manager->persist($results);
+
+
+                $results = $this->getDoctrine()->getRepository(Result::class)->findOneBy(['candidate' => $candidate, 'criteria' => 2]);
+                $results->setOralreview($oral2);
+                $manager = $this->getDoctrine()->getManager();
+                $manager->persist($results);
+
+
+                $results = $this->getDoctrine()->getRepository(Result::class)->findOneBy(['candidate' => $candidate, 'criteria' => 3]);
+                $results->setOralreview($oral3);
+                $manager = $this->getDoctrine()->getManager();
+                $manager->persist($results);
+
+
+                $results = $this->getDoctrine()->getRepository(Result::class)->findOneBy(['candidate' => $candidate, 'criteria' => 4]);
+                $results->setOralreview($oral4);
+                $manager = $this->getDoctrine()->getManager();
+                $manager->persist($results);
+
+                $manager->flush();
+            }
+
+
+            $result = $this->getDoctrine()->getRepository(Result::class)->findBy(['candidate' => $candidate]);
         }
 
         return $this->render('candidate/oneCandidate.html.twig', [
             'candidate' => $candidate,
-            'results' => $results
+            'results' => $result,
+            'request' => $request->getMethod()
         ]);
     }
 }
