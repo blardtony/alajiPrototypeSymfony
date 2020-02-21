46bbcf0ee4bfa0569128dcd5ab8dae383f95479f
Tony Blard <tonyblard55@gmail.com>
2020-02-21 10:36:59 +0100

    convert to pdf summary

--- src/Controller/CandidateController.php
+++ src/Controller/CandidateController.php
@@ -103,36 +103,31 @@ class CandidateController extends AbstractController
     {
 
         $candidate = $this->getDoctrine()->getRepository(Candidate::class)->findOneBy(['fullname' => $nameC]);
+        $name = $candidate->getFullname();
+        $strippedName = str_replace(' ', '', $name);
+        
+        //https://ourcodeworld.com/articles/read/799/how-to-create-a-pdf-from-html-in-symfony-4-using-dompdf Pour le pdf
         if ($request->isMethod('POST')) {
             //Configure Dompdf according to your needs
             $pdfOptions = new Options();
             $pdfOptions->set('defaultFont', 'Arial');
-
             // Instantiate Dompdf with our options
             $dompdf = new Dompdf($pdfOptions);
-
             // Retrieve the HTML generated in our twig file
             $html = $this->renderView('candidate/pdfSummary.html.twig', [
                 'candidate' => $candidate
             ]);
-            
-
-
             // Load HTML to Dompdf
             $dompdf->loadHtml($html);
-
             // (Optional) Setup the paper size and orientation 'portrait' or 'portrait'
             $dompdf->setPaper('A4', 'portrait');
-
             // Render the HTML as PDF
             $dompdf->render();
-
             // Output the generated PDF to Browser (force download)
-            $dompdf->stream("mypdf.pdf", [
+            $dompdf->stream("Récapitulatif_$strippedName.pdf", [
                 "Attachment" => true
             ]);
         }
-
         return $this->render('candidate/summary.html.twig', [
             'candidate' => $candidate,
         ]);
